ba2aaf93dc6c62e1493d3d71c289a1b181355600
ramkumar <ramviru9@gmail.com>
2022-07-13 13:25:30 +0530

    commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const cors=require("cors");
 const mongodb=require("mongodb");
 const app=express();
 app.use(cors({
-    origin:"https://resilient-mooncake-8315b2.netlify.app/",
+    origin:"https://resilient-mooncake-8315b2.netlify.app/"
 }))
 app.use(express.json());
 const mongoclient=mongodb.MongoClient;
